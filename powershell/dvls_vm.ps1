49048393b955d732ced04ac964e254d670e1251e
Marc-André Moreau <marcandre.moreau@gmail.com>
2021-10-29 15:04:40 -0400

    DVLS installation + configuration (#10)
    * create IIS site binding, install DVLS console
    
    * update DVLS installation
    
    * update DVLS installation
    
    * disable server manager, WAC prompt, activation overlay
    
    * update DVLS installation
    
    * Revert "disable server manager, WAC prompt, activation overlay"
    
    This reverts commit 297c9f68ebeebf92f152c3d3132ffcad407ff4a4.
    
    Co-authored-by: Marc-André Moreau <mamoreau@devolutions.net>

--- powershell/dvls_vm.ps1
+++ powershell/dvls_vm.ps1
@@ -74,6 +74,7 @@ Write-Host "Installing SQL Server Express"
 Invoke-Command -ScriptBlock {
     choco install -y --no-progress sql-server-express
     choco install -y --no-progress sql-server-management-studio
+    Install-Module -Name SqlServer -Scope AllUsers -AllowClobber -Force
 } -Session $VMSession
 
 Write-Host "Creating SQL database for DVLS"
@@ -85,21 +86,48 @@ $SqlUsername = "dvls"
 $SqlPassword = "sql123!"
 
 Invoke-Command -ScriptBlock { Param($DatabaseName, $SqlInstance)
-    Install-Module -Name SqlServer -Scope AllUsers -AllowClobber -Force
     Import-Module SqlServer -Force
     $SqlServer = New-Object Microsoft.SqlServer.Management.Smo.Server($SqlInstance)
     $SqlServer.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Mixed
     $SqlServer.Alter()
     $Database = New-Object Microsoft.SqlServer.Management.Smo.Database($SqlServer, $DatabaseName)
     $Database.Create()
+    $Database.RecoveryModel = "simple"
+    $Database.Alter()
+    Get-Service -Name 'MSSQL$SQLEXPRESS' | Restart-Service
+    Start-Sleep -Seconds 2
 } -Session $VMSession -ArgumentList @($DatabaseName, $SqlInstance)
 
-Invoke-Command -ScriptBlock { Param($SqlInstance, $SqlUsername, $SqlPassword)
+Write-Host "Creating SQL user for DVLS"
+
+Invoke-Command -ScriptBlock { Param($DatabaseName, $SqlInstance, $SqlUsername, $SqlPassword)
     Import-Module SqlServer -Force
     $SecurePassword = ConvertTo-SecureString $SqlPassword -AsPlainText -Force
     $SqlCredential = New-Object System.Management.Automation.PSCredential @($SqlUsername, $SecurePassword)
-    Add-SqlLogin -ServerInstance $SqlInstance -LoginPSCredential $SqlCredential -LoginType SqlLogin -Enable
-} -Session $VMSession -ArgumentList @($SqlInstance, $SqlUsername, $SqlPassword)
+    $Params = @{
+        ServerInstance = $SqlInstance;
+        LoginPSCredential = $SqlCredential;
+        LoginType = "SqlLogin";
+        GrantConnectSql = $true;
+        Enable = $true;
+    }
+    Add-SqlLogin @Params
+    $SqlServer = New-Object Microsoft.SqlServer.Management.Smo.Server($SqlInstance)
+    $Database = $SqlServer.Databases[$DatabaseName]
+    $Database.SetOwner('sa')
+    $Database.Alter()
+    $User = New-Object Microsoft.SqlServer.Management.Smo.User($Database, $SqlUsername)
+    $User.Login = $SqlUsername
+    $User.Create()
+    $Role = $Database.Roles['db_owner']
+    $Role.AddMember($SqlUsername)
+} -Session $VMSession -ArgumentList @($DatabaseName, $SqlInstance, $SqlUsername, $SqlPassword)
+
+$DvlsVersion = "2021.2.10.0"
+$DvlsPath = "C:\inetpub\dvlsroot"
+$DvlsAdminUsername = "dvls-admin"
+$DvlsAdminPassword = "dvls-admin123!"
+$DvlsAdminEmail = "admin@ad.it-help.ninja"
 
 $DvlsHostName = "dvls.$DomainName"
 $CertificateFile = "~\Documents\cert.pfx"
@@ -118,3 +146,85 @@ Request-DLabCertificate $VMName -VMSession $VMSession `
     -CommonName $DvlsHostName `
     -CAHostName $CAHostName -CACommonName $CACommonName `
     -CertificateFile $CertificateFile -Password $CertificatePassword
+
+Write-Host "Creating Devolutions Server IIS site"
+
+Invoke-Command -ScriptBlock { Param($DvlsHostName, $DvlsPath, $CertificateFile, $CertificatePassword)
+    $DvlsPort = 443;
+    New-Item -Path $DvlsPath -ItemType 'Directory' -ErrorAction SilentlyContinue | Out-Null
+    $CertificatePassword = ConvertTo-SecureString $CertificatePassword -AsPlainText -Force
+    $Params = @{
+        FilePath          = $CertificateFile;
+        CertStoreLocation = "cert:\LocalMachine\My";
+        Password          = $CertificatePassword;
+        Exportable        = $true;
+    }
+    $Certificate = Import-PfxCertificate @Params
+    $CertificateThumbprint = $Certificate.Thumbprint
+    $BindingInformation = '*:' + $DvlsPort + ':' + $DvlsHostName
+    $Params = @{
+        Name = "DVLS";
+        Protocol = "https";
+        SslFlag = "Sni";
+        PhysicalPath = $DvlsPath;
+        BindingInformation = $BindingInformation;
+        CertStoreLocation = "cert:\LocalMachine\My";
+        CertificateThumbprint = $CertificateThumbprint;
+    }
+    New-IISSite @Params
+} -Session $VMSession -ArgumentList @($DvlsHostName, $DvlsPath, $CertificateFile, $CertificatePassword)
+
+Write-Host "Installing Devolutions Console"
+
+Invoke-Command -ScriptBlock { Param($DvlsVersion)
+    $ProgressPreference = 'SilentlyContinue'
+    $DownloadBaseUrl = "https://cdn.devolutions.net/download"
+    $DvlsConsoleExe = "$(Resolve-Path ~)\Documents\Setup.DVLS.Console.exe"
+    Invoke-WebRequest "$DownloadBaseUrl/Setup.DPS.Console.${DvlsVersion}.exe" -OutFile $DvlsConsoleExe
+    Start-Process -FilePath $DvlsConsoleExe -ArgumentList @('/qn') -Wait
+} -Session $VMSession -ArgumentList @($DvlsVersion)
+
+Write-Host "Install Devolutions Server"
+
+Invoke-Command -ScriptBlock { Param($DvlsVersion, $DvlsPath,
+    $SqlInstance, $SqlUsername, $SqlPassword,
+    $DvlsAdminUsername, $DvlsAdminPassword, $DvlsAdminEmail)
+
+    $ProgressPreference = 'SilentlyContinue'
+    $DownloadBaseUrl = "https://cdn.devolutions.net/download"
+    $DvlsWebAppZip = "$(Resolve-Path ~)\Documents\DVLS.${DvlsVersion}.zip"
+
+    if (-Not $(Test-Path -Path $DvlsWebAppZip -PathType 'Leaf')) {
+        Invoke-WebRequest "$DownloadBaseUrl/RDMS/DVLS.${DvlsVersion}.zip" -OutFile $DvlsWebAppZip
+    }
+
+    $BackupKeysPassword = "DvlsBackupKeys123!"
+    $BackupKeysPath = "$(Resolve-Path ~)\Documents\DvlsBackupKeys"
+    New-Item -Path $BackupKeysPath -ItemType 'Directory' -ErrorAction SilentlyContinue | Out-Null
+
+    $DvlsConsoleArgs = @(
+        "server", "install",
+        "-v", "--acceptEula", "-q",
+        "--adminUsername=$DvlsAdminUsername",
+        "--adminPassword=$DvlsAdminPassword",
+        "--adminEmail=$DvlsAdminEmail",
+        "--installZip=`"$DvlsWebAppZip`"",
+        "--dps-path=`"$DvlsPath`""
+        "--website=`"DVLS`"",
+        "--serverName=`"Devolutions Server`"",
+        "--backupKeysPath=`"$BackupKeysPath`"",
+        "--backupKeysPassword=$BackupKeysPassword",
+        "--databaseHost=$SqlInstance",
+        "--databaseName=`"dvls`"",
+        "--db-username=$SqlUsername",
+        "--db-password=$SqlPassword",
+        "--disableEncryptConfig",
+        "--disablePassword")
+
+    $DvlsConsoleCli = "${Env:ProgramFiles(x86)}\Devolutions\Devolutions Server Console\DPS.Console.CLI.exe"
+
+    & $DvlsConsoleCli @DvlsConsoleArgs
+
+} -Session $VMSession -ArgumentList @($DvlsVersion, $DvlsPath,
+    $SqlInstance, $SqlUsername, $SqlPassword,
+    $DvlsAdminUsername, $DvlsAdminPassword, $DvlsAdminEmail)
