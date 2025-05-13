0bfb575d796065f2e0a4853e030b30e0510e8e87
Marc-André Moreau <mamoreau@devolutions.net>
2025-05-13 11:53:19 -0400

    update scripts

--- azure/customizers/skillable-integration-services.ps1
+++ azure/customizers/skillable-integration-services.ps1
@@ -1,3 +1,4 @@
+$ProgressPreference = 'SilentlyContinue'
 $RepoName = "Skillable-Integration-Service"
 $ZipFile = "${RepoName}.zip"
 $DownloadUrl = "https://github.com/LearnOnDemandSystems/${RepoName}/archive/refs/heads/main.zip"
@@ -6,6 +7,8 @@ Remove-Item $OutputPath -Recurse -ErrorAction SilentlyContinue | Out-Null
 Invoke-WebRequest -Uri $DownloadUrl -OutFile $ZipFile
 Expand-Archive -Path $ZipFile -DestinationPath $Env:TEMP
 Remove-Item $ZipFile | Out-Null
+Remove-Item "C:\VmIntegrationService" -Recurse -ErrorAction SilentlyContinue | Out-Null
 Expand-Archive "$OutputPath\VmIntegrationService.zip" -DestinationPath "C:\VmIntegrationService"
+Unblock-File "$OutputPath\install.ps1"
 & "$OutputPath\install.ps1"
 Remove-Item $OutputPath -Recurse | Out-Null
